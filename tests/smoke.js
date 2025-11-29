5de52faab9d39b216c2d70b5e190c5a9a721097a
Maisa Pires de Andrade <130785414+my4wyy@users.noreply.github.com>
2025-11-29 15:12:36 -0300

    Create smoke.js

---
+++ tests/smoke.js
@@ -0,0 +1,15 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export const options = {
+  vus: 1,
+  duration: '30s',
+};
+
+export default function () {
+  const res = http.get('http://localhost:3000/health');
+  check(res, {
+    'status é 200': (r) => r.status === 200,
+  });
+  sleep(1);
+}
